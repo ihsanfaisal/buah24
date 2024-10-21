fe9204d321a79baa077ea83b037d6a379de5d401
ihsanfaisal <ihsanfaisal@gmail.com>
2024-10-21 10:45:41 +0700

    Menambah fungsi tambah data buah

---
+++ main.js
@@ -0,0 +1,44 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+// ini adalah konfigurasi firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
+  authDomain: "insan-cemerlang.firebaseapp.com",
+  projectId: "insan-cemerlang",
+  storageBucket: "insan-cemerlang.appspot.com",
+  messagingSenderId: "579109661574",
+  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
+};
+
+// Inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+export async function tambahBuah(nama, warna, harga) {
+  try {
+    // menyimpan data ke firebase
+    const refDokumen = await addDoc(collection(basisdata, "buah"), {
+      nama: nama,
+      warna: warna,
+      harga: harga
+    })
+
+    // menampilkan pesan berhasil
+    console.log("berhasil menyimpan data buah")
+  } catch (error) {
+    // menampilkan pesan gagal
+    console.log("gagal menyimpan data buah")
+  }
+}
